845a04c3ea71fb484403c6b1bc4335c3857469d9
Hunter <WilsHunt604@gotvdsb.ca>
2022-11-16 09:24:24 -0500

    Kian is mad

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,18 @@ namespace Monogame_Thing_2___Hunter
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D circleTexture;
+        private Texture2D rectangleTexture;
+        private Rectangle Head;
+        private Rectangle Mouth;
+        private Rectangle Nose;
+        private Rectangle eyeOne;
+        private Rectangle eyeTwo;
+        private Rectangle eyeOneDot;
+        private Rectangle eyeTwoDot;
+        private Rectangle eyeOneOutline;
+        private Rectangle eyeTwoOutline;
+        SpriteFont TitleFont;
 
         public Game1()
         {
@@ -18,16 +30,28 @@ namespace Monogame_Thing_2___Hunter
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            Head = new Rectangle(275, 25, 300, 300);
+            Mouth = new Rectangle(375, 250, 100, 20);
+            Nose = new Rectangle(415, 170, 20, 20);
+            eyeOne = new Rectangle(300, 100, 50, 50);
+            eyeTwo = new Rectangle(500, 100, 50, 50);
+            eyeOneDot = new Rectangle(315, 115, 20, 20);
+            eyeTwoDot = new Rectangle(515, 115, 20, 20);
+            eyeOneOutline = new Rectangle(295, 95, 60, 60);
+            eyeTwoOutline = new Rectangle(495, 95, 60, 60);
+            this.Window.Title = "Hunter's Drawing";
+            _graphics.PreferredBackBufferWidth = (800);
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            circleTexture = Content.Load<Texture2D>("circle");
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
+            TitleFont = Content.Load<SpriteFont>("TitleFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +59,24 @@ namespace Monogame_Thing_2___Hunter
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(TitleFont, "Hello There!", new Vector2(100, 0), Color.Black);
+            _spriteBatch.Draw(circleTexture, Head, Color.Yellow);
+            _spriteBatch.Draw(rectangleTexture, Nose, Color.Black);
+            _spriteBatch.Draw(rectangleTexture, Mouth, Color.Black);
+            _spriteBatch.Draw(circleTexture, eyeOneOutline, Color.Black);
+            _spriteBatch.Draw(circleTexture, eyeTwoOutline, Color.Black);
+            _spriteBatch.Draw(circleTexture, eyeOne, Color.White);
+            _spriteBatch.Draw(circleTexture, eyeTwo, Color.White);
+            _spriteBatch.Draw(circleTexture, eyeOneDot, Color.Brown);
+            _spriteBatch.Draw(circleTexture, eyeTwoDot, Color.Brown);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
